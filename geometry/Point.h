9592bac035cf3901790065393f7a867301a1fb31
usher <>
2006-03-21 01:27:28 +0000

    Updates for new external packages - CLHEP, Gaudi, G4, Root, etc.

--- geometry/Point.h
+++ geometry/Point.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/geometry/geometry/Point.h,v 1.3 2001/10/06 03:37:05 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/geometry/geometry/Point.h,v 1.4 2002/01/02 02:56:45 burnett Exp $
 //   Author: T. Burnett
 //  Project: geometry
 //
@@ -11,17 +11,17 @@
 /** Defines a geometrical point, differing from  Vector only by
   its transformation properties and operators: it is illegal to add two points, for example.
   */
-class Point: public Hep3Vector , public GeomObject
+class Point: public CLHEP::Hep3Vector , public GeomObject
 {
 public:
-    Point(const Point& p):Hep3Vector(p),GeomObject(){}
-    Point():Hep3Vector(0,0,0){}
-    Point(double x, double y, double z):Hep3Vector(x,y,z){}
+    Point(const Point& p):CLHEP::Hep3Vector(p),GeomObject(){}
+    Point():CLHEP::Hep3Vector(0,0,0){}
+    Point(double x, double y, double z):CLHEP::Hep3Vector(x,y,z){}
     // constructors: default is null
     
-    Point& operator=(const Point& v){return (Point&)Hep3Vector::operator=(v);}
-    Point& operator+=(const Vector& v){return (Point&)Hep3Vector::operator+=(v);}
-    Point& operator-=(const Vector& v){return (Point&)Hep3Vector::operator-=(v);}
+    Point& operator=(const Point& v){return (Point&)CLHEP::Hep3Vector::operator=(v);}
+    Point& operator+=(const Vector& v){return (Point&)CLHEP::Hep3Vector::operator+=(v);}
+    Point& operator-=(const Vector& v){return (Point&)CLHEP::Hep3Vector::operator-=(v);}
     // assignment operators: allow  V = P - P, P = P + V, P = P - V
     
     //(uneeded?) operator Vector()const{return Vector(x(),y(),z());}
@@ -35,14 +35,14 @@ public:
     void printOn(std::ostream& f =std::cout)const;
     
 private:
-    // make illegal operations unaccessible that would inherit from Hep3Vector
+    // make illegal operations unaccessible that would inherit from CLHEP::Hep3Vector
     Point operator*(double ){return Point();}  // no multiply
     Point operator/(double ){return Point();}  // or divide
     Point operator+(const Point& ){return Point();}; // or add another Point
     
 };
 
-// resolve ambiguity bet. GeomObject and Hep3Vector
+// resolve ambiguity bet. GeomObject and CLHEP::Hep3Vector
 inline std::ostream& operator<<(std::ostream& out, const Point& v)
 {  v.printOn(out); return out;}
 
