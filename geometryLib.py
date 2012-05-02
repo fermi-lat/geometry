4f332dcb85c91dd7691fdb4f348ec5b218f378b2
jrb <>
2012-05-02 22:57:22 +0000

    geometry pkg has dependence on clhep

--- geometryLib.py
+++ geometryLib.py
@@ -1,7 +1,7 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/geometry/geometryLib.py,v 1.1 2008/07/09 21:13:46 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['geometry'])
-
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
