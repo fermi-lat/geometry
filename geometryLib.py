27df89ef2c28c84f3ef9f0634744a0a83081f4d7
jrb <>
2012-08-17 00:02:02 +0000

    for Windows no-installed-headers

--- geometryLib.py
+++ geometryLib.py
@@ -1,7 +1,14 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/geometry/geometryLib.py,v 1.1 2008/07/09 21:13:46 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/geometry/geometryLib.py,v 1.2 2012/05/02 22:57:22 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['geometry'])
+        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+            env.Tool('findPkgPath', package = 'geometry') 
     env.Tool('addLibrary', library = env['clhepLibs'])
+    # For the time being don't need incsOnly branch since geometry source
+    # doesn't reference other packages
+    #if kw.get('incsOnly', 0) == 1: 
+    #    env.Tool('findPkgPath', package = ...
+
 def exists(env):
     return 1;
